555c34a1a8b00876c67a64afd00437d5ea5e2f8b
marcus <marcus@snapzio.com>
2019-10-01 15:04:42 +0800

    ...

--- src/components/core/AppStateContext.js
+++ src/components/core/AppStateContext.js
@@ -1,52 +1,7 @@
 import React, { useReducer } from 'react';
+import reducer from '../../reducer';
 
 const AppStateContext = React.createContext([{}, () => {}]);
-
-const reducer = (state, { type, payload }) => {
-	console.log(type, payload);
-	switch (type) {
-		case 'SIGN_UP':
-			return {
-				...state,
-				userInfo: payload.user,
-				loggedUser: { username: payload.user.username },
-			};
-
-		case 'LOG_IN':
-			return {
-				...state,
-				loggedUser: {
-					...payload,
-				},
-			};
-
-		case 'SETUP_FUND_METHOD':
-			return {
-				...state,
-				users: [...state.users, payload.user],
-				userInfo: payload.user,
-			};
-
-		case 'SETUP_ACCOUNT_TYPE':
-			return {
-				...state,
-				activeType: payload.type,
-			};
-
-		case 'ADD_CONTRIBUTION':
-			return {
-				...state,
-				contributions: {
-					...state.contributions,
-					list: [...state.contributions.list, payload.contribution],
-					total: state.contributions.total + parseInt(payload.amount),
-				},
-			};
-
-		default:
-			return state;
-	}
-};
 
 const AppStateProvider = props => {
 	const initialState = {
